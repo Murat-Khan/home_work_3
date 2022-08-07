ffd3c0aa22345bc9e4a6c6841f225cefe0ad95f2
Murat-Khan <kayfff995@gmail.com>
2022-08-07 22:41:35 +0600

    fix code

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,37 @@ public class Main {
     public static void main(String[] args) {
         double [] nums = {9.7, 56.3, -7.0, 6.0, -8.0, 5.9, 6.8,
                 -23.23, -13.34, 42.6, -32.0, -78.65, 19.98, 75.5, -54.6};
-        boolean find = false;
-        double tr = nums[3];
-
-        double allNum = 0.0;
-        int index = 0;
-        for (double element: nums) {
-            if (element < 0){
-                find = true;
-                continue;
-            }
 
 
-            if (find && element > 0){
-                index = index + 1;
-                allNum = allNum + element;
-            }
 
-        }
-        System.out.println("result: " + allNum / index);
-        System.out.println("index: " + index);
-
+        System.out.println("result: " + calculateMean(nums));
         System.out.println(Arrays.toString(sortArray(nums)));
 
 
         }
 
-     
+
+        public static double calculateMean(double[] nums){
+            boolean find = false;
+            double allNum = 0.0;
+            int index = 0;
+            for (double element: nums) {
+                if (element < 0){
+                    find = true;
+                    continue;
+                }
+
+
+                if (find && element > 0){
+                    index = index + 1;
+                    allNum = allNum + element;
+                }
+
+            }
+
+            return allNum / index;
+        }
+
         public static double[] sortArray(double[] nums){
             for (int i = 0; i <nums.length ; i++) {
 
